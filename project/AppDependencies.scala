ae4a262b2313a0bac76d10dc871a40a87234e717
Jordan Rowe <37838443+jordanrowe@users.noreply.github.com>
2025-02-28 09:33:55 +0000

    Update AppDependencies.scala

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,8 +4,8 @@ import sbt.Keys.libraryDependencies
 import sbt._
 
 object AppDependencies {
-  private val bootstrapVersion = "9.4.0"
-  private val hmrcMongoVersion = "2.2.0"
+  private val bootstrapVersion = "9.10.0"
+  private val hmrcMongoVersion = "2.5.0"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
